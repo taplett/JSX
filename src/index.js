ff056cb1b77c84928aaeda4d82aebcf2a2a0f86e
Paul Taplett <taplett@gmail.com>
2020-08-20 05:41:02 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Heading</h1>
+    <ul>
+      <li>Wake up</li>
+      <li>Coffee</li>
+      <li>Code</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
