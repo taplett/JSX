33d0b1352ffbf761dd01100a84910247177e03df
Paul Taplett <taplett@gmail.com>
2020-08-21 04:29:06 -0500

    img inclusion

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const img = "https://picsum.photos/200";
+
 ReactDOM.render(
   <div>
-    <h1>Routine</h1>
-    <ul>
-      <li>Wake up</li>
-      <li>Coffee</li>
-      <li>Code</li>
-    </ul>
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      Routine
+    </h1>
+    <div>
+      <img alt="" src={img} />
+      <img className="routineImage" alt="" src="https://picsum.photos/200" />
+      <img className="routineImage" alt="" src="https://picsum.photos/200" />
+      <img className="routineImage" alt="" src="https://picsum.photos/200" />
+    </div>
   </div>,
   document.getElementById("root")
 );
